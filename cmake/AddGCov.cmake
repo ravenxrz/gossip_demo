9b65244e4e3f2bac92f7b276bc09dbaade0f17b8
ravenxrz <zhang.xingrui@foxmail.com>
2023-12-31 16:25:48 +0800

    feature(tool): add asan and gocv

---
+++ cmake/AddGCov.cmake
@@ -0,0 +1,15 @@
+function(AddCoverage target)
+  find_program(LCOV_PATH lcov REQUIRED)
+  find_program(GENHTML_PATH genhtml REQUIRED)
+  add_custom_target(coverage-${target}
+    COMMAND ${LCOV_PATH} -d . --zerocounters
+    COMMAND $<TARGET_FILE:${target}>
+    COMMAND ${LCOV_PATH} -d . --capture -o coverage.info
+    COMMAND ${LCOV_PATH} -r coverage.info '/usr/include/*'
+                         -o filtered.info
+    COMMAND ${GENHTML_PATH} -o coverage-${target}
+                            filtered.info --legend
+    COMMAND rm -rf coverage.info filtered.info
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+  )
+endfunction()
